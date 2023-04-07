361431627e39e7bac6460976a426ddea804d4c2e
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2023-04-07 18:20:56 +0300

    Final tuning, renamed tonality to mode, added output files to vcs

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import ccxt
 import sf2_loader as sf
 import matplotlib.pyplot as plt
 from matplotlib import animation
-import random
 import moviepy.editor as mpe
 
 eot = midi.EndOfTrackEvent(tick=1)
@@ -45,9 +44,9 @@ def project_data_to_range(data, start, end):
     return pitches
 
 
-def data_to_pitches_in_scale(data, min_pitch, max_pitch, tonality):
+def data_to_pitches_in_scale(data, min_pitch, max_pitch, mode):
     maximum, minimum = np.max(data), np.min(data)
-    notes = get_scale(min_pitch, max_pitch, tonality)
+    notes = get_scale(min_pitch, max_pitch, mode)
 
     step = (maximum - minimum) / len(notes)
     shift = minimum // step
@@ -79,15 +78,15 @@ def single_track_variable_volume(pitches, volume_data, channel, tick):
     return track
 
 
-def get_scale(start_pitch, end_pitch, tonality):
+def get_scale(start_pitch, end_pitch, mode):
     result = []
 
     if start_pitch < 0 or start_pitch > end_pitch:
         return result
 
-    if tonality == "DUR":
+    if mode == "DUR":
         seq = DUR_SEQ
-    elif tonality == "MOLL":
+    elif mode == "MOLL":
         seq = MOLL_SEQ
     else:
         return result
@@ -172,23 +171,27 @@ def visualize_pitches(pitches_list, volumes_list, labels, colors, interval, file
     write_video(ani, interval, filename)
 
 
-def visualize_data_separately(data_list, labels, colors, interval, filename, names):
-    track_number = len(pitches_list)
+def visualize_data_separately(data_list, labels, colors, interval, filename, names, start_date, timeframe):
+    track_number = len(data_list)
     if track_number == 0:
         return
 
     length = len(data_list[0][0])
-    # x = [datetime.datetime.fromtimestamp(data_list[0][0, i] / 1000)for i in range(length)]
     x = np.arange(length)
     y = [data_list[i][4] for i in range(track_number)]
 
-    fig, axs = plt.subplots(track_number, layout="constrained")
+    fig, axs = plt.subplots(track_number, layout="tight")
+    if track_number == 1:
+        axs = [axs]
 
     for i in range(track_number):
         axs[i].set(ylabel="Price", title=(labels[i] + ' — ' + names[i]))
         data_min, data_max = np.min(data_list[i][4]), np.max(data_list[i][4])
         padding = (data_max - data_min) * 0.05
         axs[i].axis([0, len(x), data_min - padding, data_max + padding])
+
+    fig.suptitle(f"From {start_date.split('+')[0]}, {timeframe} intervals,\n {length} points", fontsize=18)
+    fig.tight_layout()
 
     lines = [axs[i].plot([], [], color=colors[i], label=labels[i])[0] for i in range(track_number)]
     areas = [project_data_to_range(data_list[i][5], 2, 20) for i in range(track_number)]
@@ -211,9 +214,9 @@ def get_data(pairs, timeframe, start, limit):
     return [parse_rates_data(pairs[i], timeframe, start, limit) for i in range(len(pairs))]
 
 
-def get_pattern(data, min_pitch, max_pitch, tonality, min_velocity, max_velocity, tick, resolution):
+def get_pattern(data, min_pitch, max_pitch, mode, min_velocity, max_velocity, tick, resolution):
     data_len = len(data)
-    pitches_list = [data_to_pitches_in_scale(data[i][4], min_pitch, max_pitch, tonality) for i in range(data_len)]
+    pitches_list = [data_to_pitches_in_scale(data[i][4], min_pitch, max_pitch, mode) for i in range(data_len)]
     velocity_data = [project_data_to_range(data[i][5], min_velocity, max_velocity) for i in range(data_len)]
 
     pattern = midi.Pattern(
@@ -227,32 +230,41 @@ if __name__ == '__main__':
     # test_sound()
 
     # Parameters for making pattern
-    filename = "output/result.mid"
-
-    timeframe = '1h'
-    start = "2023-01-24 11:20:00+00:00"
-    limit = 30
+    timeframe = '1d'
+    start = "2023-01-24 00:00:00+00:00"
+    limit = 50
     pairs = [
         'BTC/USDT',
         'ETH/USDT',
-        'SOL/USDT',
+        # 'SOL/USDT',
     ]
     data = get_data(pairs, timeframe, start, limit)
 
     min_pitch, max_pitch = 31, 91
     min_velocity, max_velocity = 80, 127
-    tonality = "MOLL"
+    mode = "DUR"
     tick = 100
     resolution = 100
-    pattern, pitches_list, velocity_data = get_pattern(data, min_pitch, max_pitch, tonality, min_velocity, max_velocity,
+    pattern, pitches_list, velocity_data = get_pattern(data, min_pitch, max_pitch, mode, min_velocity, max_velocity,
                                                        tick, resolution)
 
+    audio_const = f"{min_pitch}_{max_pitch}_{min_velocity}_{max_velocity}_{mode}"
+    start_date = start.split()
+
+    time = start_date[1].split('+')[0].split(':')
+    video_const = f"{start_date[0]}_{time[0]}-{time[1]}-{time[2]}_{timeframe}_{limit}"
+
+    cryptocurrencies = '_'.join(pairs).replace('/', '-')
+
+    const = video_const + '_' + audio_const + '_' + cryptocurrencies
+
+    filename = "output/midi/" + const + '.mid'
     # Write the MIDI
     if pattern == -1:
         exit(-1)
     midi.write_midifile(filename, pattern)
 
-    r = lambda: random.randint(0, 255)
+    # r = lambda: random.randint(0, 255)
     # colors = [f'#{r():02x}{r():02x}{r():02x}' for i in range(len(pairs))]
     colors = ['red', 'green', 'blue', 'orange', 'blueviolet', 'magenta', 'lime']
 
@@ -266,14 +278,16 @@ if __name__ == '__main__':
         # instruments_names.append(all_instruments.get(0).get(i - 1))
         instruments_names.append(loader.get_instrument_name(1, 0, i - 1))
 
-    output_video_file = 'output/result.mp4'
+    output_video_file = 'output/temp.mp4'
     # visualize_data(pitches_list, velocity_data, pairs, colors, tick, output_video_file)
-    visualize_data_separately(data, pairs, colors, tick, output_video_file, instruments_names)
+    visualize_data_separately(data, pairs, colors, tick, output_video_file, instruments_names, start, timeframe)
 
-    audio_file = 'output/result'
+    audio_file = 'output/audio/' + const
     ext = 'mp3'
-    # loader.play_midi_file(current_chord='output/result.mid', instruments=[1, 72])
+    # loader.play_midi_file(current_chord=filename, instruments=[1, 72])
     loader.export_midi_file(filename, name=audio_file + '.' + ext, format=ext, instruments=instruments)
 
-    final = 'output/final.mp4'
+
+
+    final = 'output/video/' + const + '.mp4'
     create_final_video(audio_file + '.' + ext, output_video_file, final)
